7848521c9d571c07c6dcd2f68f47c1879d46fb75
Amrit Deep <amrit@cratebind.com>
2016-02-29 12:57:39 -0600

    Able to authenticate and access data

--- Demo.rb
+++ Demo.rb
@@ -1,6 +1,48 @@
 require 'savon'
+require "gyoku"
+require 'pry'
 
-class Demo
+# class Demo
 
 
+# end
+# client = Savon.client(wsdl: "http://mail.frontierutilities.com:50388/EnrollAPI/WebAPI.asmx?wsdl")
+client = Savon.client do
+  wsdl "http://mail.frontierutilities.com:50388/EnrollAPI/WebAPI.asmx?wsdl"
+  convert_request_keys_to :none
+  env_namespace :soapenv
+  pretty_print_xml true  
 end
+
+## Authentication
+response = client.call(:authentication, message: { 
+	'Authenticate' => {
+		'UserName' => 'CMU_01',
+		'Password' => 'UjtE5581'
+	}
+	})
+
+# puts response.body
+
+session_id = response.body[:authentication_response][:authentication_result][:session_id]
+# data = client.call(:get_products, message: {
+# 	'GetProducts' => {
+# 		'SessionID' => session_id
+# 	}
+# 	})
+
+data = client.call(:get_products, message: {
+		'SessionID' => session_id
+	})
+
+# binding.pry
+
+product_details = data.body[:get_products_response][:get_products_result][:product_details][:product_details]
+
+product_details.each do |product|
+	# binding.pry
+	puts product[:product_title]
+end
+
+# puts data.body
+
--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 gem 'savon'
+gem 'gyoku'
 gem 'pry'
